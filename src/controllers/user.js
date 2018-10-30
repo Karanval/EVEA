004fa3ce9a9cabd1c785597746debf894332dc52
karynn <yotu.tuy@gmail.com>
2018-10-30 19:52:41 -0400

    login control

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,13 +3,10 @@ var jwt = require('jsonwebtoken');
 var token;
 process.env.SECRET_KEY = "devesh";
 
-exports.register = function(req, res) {
-  var today = new Date();
-  var appData = {
-    "error": 1,
-    "data": ""
-  };
-  var userData = {
+exports.register = async function(req, res) {
+  let today = new Date();
+  let appData = {"error":1, "data":''};
+  let userData = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
@@ -17,21 +14,21 @@ exports.register = function(req, res) {
   }
   database.connection.getConnection(function(err, connection) {
     if (err) {
-      appData["error"] = 1;
-      appData["data"] = "Error interno del servidor.";
-      appData["error_stacktrace"] = err;
-      res.status(500).json(appData);
+      appData.error = 1;
+      appData.data = "Error interno del servidor.";
+      appData.error_stacktrace = err;
+      send(res, 500, 'register', JSON.stringify(appData));
     } else {
       connection.query('SELECT email FROM user WHERE email=',[])
       connection.query('INSERT INTO user SET ?', userData, function(err, rows, fields) {
         if (!err) {
           appData.error = 0;
-          appData["data"] = "Usuario registrado correctamente.";
-          res.status(201).json(appData);
+          appData.data = "Usuario registrado correctamente.";
+          send(res, 201, 'register', JSON.stringify(appData));
         } else {
-          appData["data"] = "Ocurrió un error con la consulta a la bd.";
-          appData["error_stacktrace"] = err;
-          res.status(400).json(appData);
+          appData.data = "Ocurrió un error con la consulta a la bd.";
+          appData.error_stacktrace = err;
+          send(res, 400, 'register', JSON.stringify(appData));
         }
       });
       connection.release();
@@ -40,42 +37,41 @@ exports.register = function(req, res) {
 };
 
 exports.login = function(req, res) {
-  var appData = {};
-  var email = req.body.email;
-  var password = req.body.password;
+  let appData = {};
+  let email = req.body.email;
+  let password = req.body.password;
   database.connection.getConnection(function(err, connection) {
     if (err) {
-      appData["error"] = 1;
-      appData["data"] = "Error interno del servidor.";
-      appData["error_stacktrace"] = err;
-      res.status(500).json(appData);
+      appData.error = 1;
+      appData.data = "Error interno del servidor.";
+      appData.error_stacktrace = err;
+      send(res, 500, 'login', JSON.stringify(appData));
     } else {
       connection.query('SELECT * FROM user WHERE email = ?', [email], function(err, rows, fields) {
         if (err) {
           appData.error = 1;
-          appData["data"] = "Ocurrió un error con la consulta a la bd.";
-          appData["error_stacktrace"] = err;
-          res.status(400).json(appData);
+          appData.data = "Ocurrió un error con la consulta a la bd.";
+          appData.error_stacktrace = err;
+          send(res, 400, 'login', JSON.stringify(appData));
         } else {
           if (rows.length > 0) {
             if (rows[0].password == password) {
               let user = JSON.parse(JSON.stringify(rows[0]));
-              token = jwt.sign(user, process.env.SECRET_KEY, {
-                expiresIn: 5000
-              });
+              token = jwt.sign(user, process.env.SECRET_KEY, {});
+
               appData.error = 0;
-              appData["data"] = "Sesion iniciada";
+              appData.data = "Sesion iniciada";
               res.set("token", token);
-              res.status(200).json(appData);
+              send(res, 200, 'login', JSON.stringify(appData));
             } else {
               appData.error = 1;
-              appData["data"] = "Email o password incorrecto.";
-              res.status(204).json(appData);
+              appData.data = "Email o password incorrecto.";
+              send(res, 204, 'login', JSON.stringify(appData));
             }
           } else {
             appData.error = 1;
-            appData["data"] = "El email no está registrado.";
-            res.status(204).json(appData);
+            appData.data = "El email no está registrado.";
+            send(res, 204, 'login', JSON.stringify(appData));
           }
         }
       });
@@ -107,3 +103,8 @@ exports.getUsers = function(req, res) {
     }
   });
 };
+
+function send(res, status, file, data) {
+  res.status(status);
+  res.render(file, {data})
+}
